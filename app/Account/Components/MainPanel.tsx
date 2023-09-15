d16bac638a039f8a0d160864451c222517c05941
farah-chourou <78874349+farah-chourou@users.noreply.github.com>
2023-09-15 21:27:07 +0100

    photo fixed

--- app/Account/Components/MainPanel.tsx
+++ app/Account/Components/MainPanel.tsx
@@ -121,6 +121,9 @@ export default function MainPanel() {
       if (secondResponse.ok) {
         const responseText = await secondResponse.text();
         setSucessMSG("User information updated successfully");
+        setTimeout(() => {
+          window.location.reload();
+        }, 1000);
       } else {
         throw new Error(
           "Network response was not ok " + secondResponse.statusText
@@ -138,34 +141,26 @@ export default function MainPanel() {
   return (
     <main>
       <div className="w-[870px] h-[730px] relative bg-white rounded shadow">
-        <div className="left-[80px] top-[40px] absolute text-red-500 text-xl font-medium leading-7">
+        <div className="left-[80px]  absolute text-red-500 text-xl font-medium leading-7">
           Edit Your Profile
         </div>
 
         <form onSubmit={(e) => handleSubmit(e)}>
           <main>
-            <div className="left-[80px] top-[90px] absolute flex items-center justify-center w-40 h-40 rounded-full">
-              <label className="flex flex-col items-center justify-center w-40 h-40 rounded-full border-2 border-gray-300 border-dashed rounded-lg bg-gray-50 dark:hover:bg-bray-800 dark:bg-gray-700 hover:bg-gray-100 dark:border-gray-600 dark:hover:border-gray-500 dark:hover:bg-gray-600">
-                <div className="flex flex-col items-center justify-center pt-5 pb-6">
-                  <svg
-                    className="w-8 h-8 mb-4 text-gray-500 dark:text-gray-400"
-                    aria-hidden="true"
-                    xmlns="http://www.w3.org/2000/svg"
-                    fill="none"
-                    viewBox="0 0 20 16"
-                  >
-                    <path
-                      stroke="currentColor"
-                      stroke-linecap="round"
-                      stroke-linejoin="round"
-                      stroke-width="2"
-                      d="M13 13h3a3 3 0 0 0 0-6h-.025A5.56 5.56 0 0 0 16 6.5 5.5 5.5 0 0 0 5.207 5.021C5.137 5.017 5.071 5 5 5a4 4 0 0 0 0 8h2.167M10 15V6m0 0L8 8m2-2 2 2"
+            <div className="left-[80px] top-[70px] absolute flex items-center justify-center w-40 h-40 rounded-full">
+              <label className="flex flex-col items-center justify-center w-40 h-45 rounded-full border-2 border-gray-300 border-dashed rounded-lg bg-gray-50 dark:hover:bg-bray-800 dark:bg-gray-700 hover:bg-gray-100 dark:border-gray-600 dark:hover:border-gray-500 dark:hover:bg-gray-600">
+                <div className="flex flex-col items-center justify-center w-full h-full">
+                  <div className="flex flex-col items-center justify-center w-full h-full">
+                    <img
+                      src={userData?.data.user.photo}
+                      alt="photo de profile"
+                      className="w-full h-full mb-4 object-cover"
                     />
-                  </svg>
-                  <p className="mb-2 text-sm text-gray-500 dark:text-gray-400">
-                    <span className="font-semibold">Click to upload</span> or
-                    drag and drop
-                  </p>
+                    <p className="mb-2 text-sm text-gray-500 dark:text-gray-400">
+                      <span className="font-semibold">Click to upload</span> or
+                      drag and drop
+                    </p>
+                  </div>
                 </div>
                 <input
                   id="dropzone-file"
@@ -181,7 +176,7 @@ export default function MainPanel() {
               </label>
             </div>
           </main>
-          <div className="left-[80px] top-[254px] absolute justify-start items-start gap-12 inline-flex">
+          <div className="left-[80px] top-[274px] absolute justify-start items-start gap-12 inline-flex">
             <div className="flex-col justify-start items-start gap-2 inline-flex">
               <p className="text-black text-base font-normal leading-normal">
                 First Name
@@ -205,7 +200,7 @@ export default function MainPanel() {
               />
             </div>
           </div>
-          <div className="left-[80px] top-[340px] absolute justify-start items-start gap-12 inline-flex">
+          <div className="left-[80px] top-[358px] absolute justify-start items-start gap-12 inline-flex">
             <div className="flex-col justify-start items-start gap-2 inline-flex">
               <p className="text-black text-base font-normal leading-normal">
                 Email
@@ -230,7 +225,7 @@ export default function MainPanel() {
               />
             </div>
           </div>
-          <div className="left-[80px] top-[436px] absolute flex-col justify-start items-start gap-4 inline-flex">
+          <div className="left-[80px] top-[446px] absolute flex-col justify-start items-start gap-4 inline-flex">
             <div className="flex-col justify-start items-start gap-2 flex">
               <p className="text-black text-base font-normal leading-normal">
                 Password Changes
@@ -262,16 +257,16 @@ export default function MainPanel() {
             />
           </div>
           {passwordError && (
-            <div className="left-[80px] top-[614px] absolute text-red-500">
+            <div className="left-[80px] top-[624px] absolute text-red-500">
               {passwordError}
             </div>
           )}
           {sucessMSG && (
-            <div className="left-[80px] top-[614px] absolute text-green-500">
+            <div className="left-[80px] top-[624px] absolute text-green-500">
               {sucessMSG}
             </div>
           )}
-          <div className="left-[487px] top-[634px] absolute justify-start items-center gap-8 inline-flex">
+          <div className="left-[487px] top-[644px] absolute justify-start items-center gap-8 inline-flex">
             <Link
               href="/"
               className="text-black text-base font-normal leading-normal"
